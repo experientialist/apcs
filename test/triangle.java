7a015b9bbfdb3062743f33b7377c8115bd162805
Work station <workstation@dkoo2016.local>
2014-12-16 22:22:49 +0900

    Dec 15

---
+++ test/triangle.java
@@ -0,0 +1,13 @@
+public class triangle{
+	public static void main(String[] args) {
+		for (int i = 1; i<101; i=i+2){
+			String x="*";
+			for (int j=0 ;j<i;j++) {
+				System.out.print(x);
+			}
+			System.out.println("");
+		}
+	}
+}
+
+
